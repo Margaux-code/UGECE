988cce4b626df535d97dd58c962aaf8beaa981da
Margaux-code <63307608+Margaux-code@users.noreply.github.com>
2022-03-28 13:21:48 +0200

    Premier Commit

---
+++ POO_JavaV2/POO_JavaV2/JDBC2020/Read_me.txt
@@ -0,0 +1,1 @@
+On est partit du code fournit dans le tutoriel JDBC de campus pour construire notre cours autours de ce modèle
---
+++ POO_JavaV2/POO_JavaV2/JDBC2020/src/Model/Client.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
+
+/**
+ *
+ * @author matth
+ */
+public class Client {
+
+    private String User = null;
+    private String Mail=null;
+    private String Mdp = null;
+    private String Membre = null;
+    private String Facture = null;
+    private Boolean Connecte = false;
+    
+
+    
+
+    public void client(String us, String mail,String mdp, String Mem, String Fac) {
+        User = us;
+        Mdp = mdp;
+        Membre = Mem;
+        Facture = Fac;
+        Mail=mail;
+    }
+    public void afficheClient()
+    {
+        System.out.print("Nom:"+User+" mdp:"+Mdp+" Membre:"+Membre+" Facture:"+Facture+" mail:"+Mail);
+    }
+
+    public void setUser(String us) {
+        this.User = us;
+    }
+    public void setMail(String mail) {
+        this.Mail = mail;
+    }
+
+    public void setConnecte(Boolean Con) {
+        this.Connecte = Con;
+    }
+
+    public  void setMdp(String mdp) {
+        this.Mdp = mdp;
+    }
+
+    public void setMembre(String mem) {
+        this.Membre = mem;
+    }
+
+    public void setFacture(String fact) {
+        this.Facture = fact;
+    }
+
+    public String getUser() {
+        return User;
+
+    }
+    public String getMail() {
+        return Mail;
+
+    }
+
+    public String getMdp() {
+        return Mdp;
+
+    }
+
+    public Boolean getConnecte() {
+        return Connecte;
+    }
+
+    public String getMembre() {
+        return Membre;
+
+    }
+
+    public String getFacture() {
+        return Facture;
+
+    }
+}
