e14093d5772d35aee5548b09ee18bfbc03570410
Margaux-code <margaux.langautier@gmail.com>
2022-03-29 09:53:36 +0200

    Construction d'une salle
    C'est pas grand chose mais je l'ai perdu une fois donc là je le push

--- POO_JavaV2/POO_JavaV2/JDBC2020/src/Model/salle.java
+++ POO_JavaV2/POO_JavaV2/JDBC2020/src/Model/salle.java
@@ -11,16 +11,26 @@ import java.time.format.DateTimeFormatter;
  *
  * @author marga
  */
+/*
+--EXPLICATION DE LA CLASSE SALLE
+
+Le concept de cette classe est de permettre a un employé de créer une salle avec un film
+Puis on pourra remplir la salle avec des réservations
+
+*/
 public class salle {
 
     private int m_places_libre;
     private int m_place;    
     DateTimeFormatter m_date = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private int m_ID_film;
+    private int m_ID_salle;
 
     public salle()
 
     {
         System.out.println("yyyy/MM/dd HH:mm:ss-> " + this.m_date.format(LocalDateTime.now()));
     }
+    
 
 }
