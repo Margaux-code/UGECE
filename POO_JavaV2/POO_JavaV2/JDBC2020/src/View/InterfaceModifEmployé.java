3dead6145fc2e484efc5b04d1b584d83dcef9606
Cyriac <78726949+mencyr@users.noreply.github.com>
2022-04-03 17:28:07 +0200

    the last one

--- POO_JavaV2/POO_JavaV2/JDBC2020/src/View/InterfaceModifEmployé.java
+++ POO_JavaV2/POO_JavaV2/JDBC2020/src/View/InterfaceModifEmployé.java
@@ -125,6 +125,7 @@ public class InterfaceModifEmployé extends javax.swing.JFrame {
         );
 
         jPanel7.setBackground(new java.awt.Color(108, 122, 137));
+        jPanel7.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         jLabel3.setBackground(new java.awt.Color(108, 122, 137));
         jLabel3.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
@@ -280,9 +281,8 @@ public class InterfaceModifEmployé extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 1205, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 598, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
 
         pack();
